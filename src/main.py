e3a152141fb559b05f1403d89acffbd13ac424fc
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-11-21 04:14:28 +0300

    Feat

--- src/main.py
+++ src/main.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 from typing import Any
 
 from fastapi import FastAPI, Response
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import jinja2
 from motor.motor_asyncio import AsyncIOMotorClient
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request as StarletteRequest
 
-from src.config import create_client_from_env, get_database_from_env
+from src.config import create_client_from_env, get_database_from_env, get_directories
 from src.routers.v0 import create_router as create_api_router
+from src.routers.internal import create_router as create_internal_router
 from src.service import Service
 
 
@@ -32,6 +36,8 @@ def create_app() -> FastAPI:
 
 
 def create_app_with_config(client: AsyncIOMotorClient[Any]) -> FastAPI:
+    directories = get_directories()
+
     app = FastAPI(
         title="Leadhit task",
         version="0.1.0",
@@ -43,10 +49,25 @@ def create_app_with_config(client: AsyncIOMotorClient[Any]) -> FastAPI:
     )
     app.add_middleware(CustomCORSMiddleware)
 
+    templates = Jinja2Templates(
+        directory=directories.templates,
+        undefined=jinja2.StrictUndefined,
+    )
+
+    app.mount(
+        "/static",
+        StaticFiles(directory=directories.static),
+        name="static",
+    )
+
+
     async def get_service():
         db = get_database_from_env(client)
         service = Service(db)
         yield service
+
+    internal_router = create_internal_router(templates)
+    app.include_router(internal_router, tags=["internal"], include_in_schema=False)
 
     api_router = create_api_router(get_service)
     app.include_router(api_router, prefix="/api/v0", tags=["api", "v0"])
---
+++ src/routers/internal.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+from starlette.responses import HTMLResponse
+
+
+def create_router(
+    templates: Jinja2Templates,
+) -> APIRouter:
+    router = APIRouter()
+
+    @router.get("/", response_class=HTMLResponse)
+    async def home(request: Request):
+        return templates.TemplateResponse(
+            "index.html", {"request": request, "route": "home"}
+        )
+
+    return router
